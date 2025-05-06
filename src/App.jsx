ab4527efe22a62693c2e1758c8ded50b6301460d
Amar Rama <ramaamar@gmail.com>
2025-05-06 18:47:29 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-primary">Project Initialized</h1>
+    </>
+  )
+}
+
+export default App
